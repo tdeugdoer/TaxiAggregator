21cb53bd0ad7627f6d79d0528b315c0a0bfe11cb
tdeugdoer <tegor2003@gmail.com>
2024-09-05 23:22:30 +0300

    Edit liquibase format: yaml -> xml

--- passenger-service/src/main/resources/db/changelog/0.0.1/changeset-create-passengers-table.sql
+++ passenger-service/src/main/resources/db/changelog/0.0.1/changeset-create-passengers-table.sql
@@ -1,12 +1,8 @@
---liquibase formatted sql
-
---changeset tserashkevich:1
 CREATE TABLE passengers
 (
     id           UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     name         VARCHAR(50)        NOT NULL,
     gender       VARCHAR(5)         NOT NULL CHECK (gender IN ('Men', 'Women', 'Other')),
-    phone_number VARCHAR(12) UNIQUE NOT NULL,
+    phone_number VARCHAR(13) UNIQUE NOT NULL,
     birth_date   DATE
 );
---rollback drop table passangers;
