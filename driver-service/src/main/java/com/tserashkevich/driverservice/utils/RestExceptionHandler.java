80e1ce90e2287be2bfa400e42a444e1570579886
tdeugdoer <tegor2003@gmail.com>
2024-09-10 16:41:30 +0300

    Add controllers, localization, exception handler

---
+++ driver-service/src/main/java/com/tserashkevich/driverservice/utils/RestExceptionHandler.java
@@ -0,0 +1,56 @@
+package com.tserashkevich.driverservice.utils;
+
+import com.tserashkevich.driverservice.dtos.ExceptionResponse;
+import com.tserashkevich.driverservice.dtos.ValidationErrorResponse;
+import com.tserashkevich.driverservice.dtos.Violation;
+import com.tserashkevich.driverservice.exceptions.CarNotFoundException;
+import com.tserashkevich.driverservice.exceptions.DriverNotFoundException;
+import jakarta.validation.ConstraintViolationException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+
+import java.util.List;
+
+@RestControllerAdvice
+@Slf4j
+public class RestExceptionHandler {
+    @ExceptionHandler({DriverNotFoundException.class, CarNotFoundException.class})
+    public ResponseEntity<ExceptionResponse> handleNotFoundException(RuntimeException ex) {
+        log.error(LogList.NOT_FOUND_ERROR, ex.getMessage());
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(new ExceptionResponse(ex.getMessage()));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ValidationErrorResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+        final List<Violation> violations = ex.getBindingResult().getFieldErrors().stream()
+                .map(error -> new Violation(error.getField(), error.getDefaultMessage()))
+                .toList();
+        log.error(LogList.METHOD_ARGUMENT_ERROR, violations);
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(new ValidationErrorResponse(violations));
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<ExceptionResponse> handleConstraintViolationException(RuntimeException ex) {
+        log.error(LogList.CONSTRAINT_VIOLATION_ERROR, ex.getMessage());
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(new ExceptionResponse(ex.getMessage()));
+    }
+
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public ResponseEntity<ExceptionResponse> handleMethodArgumentTypeMismatchException(MethodArgumentTypeMismatchException ex) {
+        log.error(LogList.METHOD_ARGUMENT_ERROR, ex.getMessage());
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(new ExceptionResponse("Wrong request parameter: " + ex.getName()));
+    }
+}
