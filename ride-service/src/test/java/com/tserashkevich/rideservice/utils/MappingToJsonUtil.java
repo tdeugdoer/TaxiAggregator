1effb590e7d742e91eb444b01c39778cb90a9d3f
tdeugdoer <tegor2003@gmail.com>
2024-10-14 18:57:02 +0300

    Added integration tests of ride service

---
+++ ride-service/src/test/java/com/tserashkevich/rideservice/utils/MappingToJsonUtil.java
@@ -0,0 +1,18 @@
+package com.tserashkevich.rideservice.utils;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class MappingToJsonUtil {
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+
+    public static String toJson(Object object) {
+        try {
+            return objectMapper.writeValueAsString(object);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
